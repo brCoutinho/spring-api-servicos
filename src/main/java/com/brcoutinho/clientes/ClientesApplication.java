6776fd45a3dd17841156508af9e0bcfcd173f3ad
Thiago Coutinho <thcoutinho1997@gmail.com>
2021-04-27 11:32:27 -0300

    Testando conexão com o bd

--- src/main/java/com/brcoutinho/clientes/ClientesApplication.java
+++ src/main/java/com/brcoutinho/clientes/ClientesApplication.java
@@ -1,10 +1,23 @@
 package com.brcoutinho.clientes;
 
+import com.brcoutinho.clientes.model.entity.Cliente;
+import com.brcoutinho.clientes.model.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ClientesApplication {
+
+	@Bean
+	public CommandLineRunner run(@Autowired ClienteRepository repository){
+		return args -> {
+			Cliente cliente = Cliente.builder().cpf("12345678912").nome("Thiago").build();
+			repository.save(cliente);
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientesApplication.class, args);
